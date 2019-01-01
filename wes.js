63385f890f494e112f95e92f4606b764d9831425
aploit123 <45037205+aploit123@users.noreply.github.com>
2019-01-02 00:06:57 +0200

    Update wes.js

--- wes.js
+++ wes.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const adminprefix = "#";
-const devs = ['498177367152263178', '498177367152263178'];
+const adminprefix = "Ab";
+const devs = ['407214903276077057', '407214903276077057'];
  
 
 client.on('message', message => {
@@ -50,4 +50,4 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 });
 
-client.login('');
+client.login(process.env.BOT_TOKEN);
